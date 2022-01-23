8e7b0e2c3f7cbbf848edf8e68362a89f8bce6a26
Shivangi Maurya <shivangimouryaceo@gmail.com>
2022-01-23 13:59:33 +0530

    commit

---
+++ src/Data.jsx
@@ -0,0 +1,154 @@
+const DATA = [
+    {
+        id : 0,
+        title : "WOMENS SKIRT - BLACK",
+        price : 85,
+        desc : " A contemporary fit mid-length skirt in pleated fabric.Elasticated waistband and back slit for a chic look. Best paired with smart tops for dressy evenings.",
+
+        img : "assets/images/products/imgg09.jpg",
+    
+    },
+    {
+        id : 1,
+        title : "ELEVATE- WOMENS PRINT DRESS",
+        price : 1189,
+        desc : "a chic short dress in ditzy floral print.V neck with double tie up, half sleeve with elastic, and a line fit with light circular ruffle at the bottom.best paired with smart heels for dates and dinners.",
+        img : "/assets/images/products/imgg2.jpeg"
+    },
+    {
+        id : 2,
+        title : "DECORA- WOMENS DRESS - BLACK",
+        price : 1295,
+        desc : "A chic halter neck midi dress with attached ruffles at the neck.Drawcord at the neck, statement ruffles, and A-line fit with side pocket.Best wore with smart footwear for dates and brunches. ",
+        img : "/assets/images/products/imgg3.jpeg",
+    },
+    {
+        id : 3,
+        title : "LIPO - WOMENS DENIM DRAWSTRING TROUSER - BLACK",
+        price : 1005,
+        desc : "A parallel fit trouser in tencil denim.with side pockets and customized detailings.A perfect pair for T shirts and casual sweaters for everyday outings. ",
+        img : "/assets/images/products/imgg3.jpeg"
+    },
+    {
+        id : 4,
+        title : "RECKON-DENIM - BLACK",
+        price : 1185,
+        desc : "Comfort fit denim ,Cotton Elasthane fabric,Best worn with tshirt or shirt,16 Stitches per inch, German thread.",
+        
+        img : "/assets/images/products/imgg4.jpg"
+    },
+    {
+        id : 5,
+        title : "PLOD-TROUSER",
+        price : 1250,
+        desc : "A wide hem straight pants with elasticated back,100% Cotton poplin thin stripes.Pair it with solid fitted tops or similar stripe top for a full coordinate look.",
+        img : "/assets/images/products/imgg6.jpg"
+    },
+    {
+        id : 6,
+        title : "Treva Dobby Multitude Stretch Jacket In Wine (Slim Fit)",
+        price : 1950,
+        desc : "Microstructure, 2 Button, Notch Lapel Travel Jacket In Slim Fit With Detachable Lapel &Amp; Travel Pockets",
+        img : "/assets/images/products/imgg7.jpg"
+    },
+    {
+        id : 7,
+        title : "AUTUMN WINTER THICK SLIM FIT PLAID WEDDING DRESS TUXEDOS",
+        price : 1990,
+        desc : "Microstructure, 2 Button, Notch Lapel Travel Jacket In Slim Fit With Detachable Lapel &Amp; Travel Pockets",
+        img : "/assets/images/products/imgg8.jpg"
+    },
+    {
+        id : 8,
+        title : "JENA SOLID EVERGREEN JACKET IN BLUE (SLIM FIT)",
+        price : 1250,
+        desc : "The 2Sb Wool Blend Jacket With Distinguishable High Stance Notch, Is About To Be A Staple In Every Sophisticated Man'S Wardrobe. Made In Our Best-Selling Fit, This Black Garment Checks Everything, Right From The Wool Blend Of Finest Quality To Provide Excellent Body Drape To The One-Serves-All Capacity Of This Sharp Jacket To Work Really Well In Multi-Occasion.",
+        img : "/assets/images/products/imgg91.jpg"
+    },
+    {
+        id : 9,
+        title : "ALASTAR DOBBY SHIRT IN INDIGO (SLIM FIT)",
+        price : 1450,
+        desc : "Indigo Solid Shirt In Semi Cut Away Collar",
+        img : "/assets/images/products/imgg10.jpg"
+    },
+    {
+        id : 10,
+        title : "NOLAN SOLID CLASSIC SHIRT",
+        price : 1650,
+        desc : " Solid Shirt In Semi Cut Away Collar",
+        img : "/assets/images/products/imgg11.jpg"
+    },
+    {
+        id : 11,
+        title : "SAM SOLID TRENDY SHIRT ",
+        price : 3150,
+        desc : " Solid Shirt In Semi Cut Away Collar",
+        img : "/assets/images/products/imgg12.jpg"
+    },
+    {
+        id : 12,
+        title : "KRAMA- WOMENS BOMBER JACKET ",
+        price : 4350,
+        desc : "A classic printed slim fit jacket with mandarin collar details.Best worn for casual outings",
+        img : "/assets/images/products/imgg13.jpg"
+    },
+    {
+        id : 13,
+        title : "BOZO- WOMENS PLAIN JACKET ",
+        price : 2250,
+        desc : "A contemporary sweatshirt in textured fabric. High-zipped neck and full sleeves. Best worn for winter paired with casual bottoms.",
+        img : "/assets/images/products/imgg14.jpg"
+    },
+    {
+        id : 14,
+        title : "NECT- WOMENS JACKET",
+        price : 1050,
+        desc : " A contemporary bomber in textured sweatshirt fabric. Full front zipper, ribbed neck, long sleeves and side pockets. Best worn for winters paired with casual bottoms.",
+        img : "/assets/images/products/imgg15.jpg"
+    },
+    {
+        id : 15,
+        title : "FAWN DESIGNER LEHENGA",
+        price : 1550,
+        desc : "A handful of glittering sparkles for the radiant you. This designer soft net lehenga choli comes in shades of mulberry purple. Sequin and thread work elevate its charm and make it your perfect pick. Choose this and slay around in glamour and style.",
+        img : "/assets/images/products/imgg16.jpg"
+    },
+    {
+        id : 16,
+        title : "PEACH ORGANZA LEHENGA CHOLI WITH DUPATTA",
+        price : 1150,
+        desc : "Party wear peach Organza Lehenga Choli comes with organza blouse piece and net Dupatta. Lehenga is semi-stitched and has cancan. ",
+        img : "/assets/images/products/imgg17.jpg"
+    },
+    {
+        id : 17,
+        title : "SOFT NET LEHENGA CHOLI WITH SEQUIN AND THREAD WORK(PRE-ORDER)",
+        price : 3250,
+        desc : "A handful of glittering sparkles for the radiant you. This designer soft net lehenga choli comes in shades of mulberry purple. Sequin and thread work elevate its charm and make it your perfect pick. Choose this and slay around in glamour and style.",
+        img : "/assets/images/products/imgg18.jpg"
+    },
+    {
+        id : 18,
+        title : "CREAM FESTIVE WEAR EMBROIDERED DOLLA SILK GOWN SUIT",
+        price : 4250,
+        desc : "This gown is made of dolla silk fabric accompanied with santoon inner & bottom & soft net dupatta. Specially designed to wear for festivals, parties, social get together and family occasions, where you want to be center of attraction. Pair it with fancy accessories to make your look more beautiful.",
+        img : "/assets/images/products/imgg19.jpg"
+    },
+    {
+        id : 19,
+        title : "AQUA BLUE PARTYWEAR EMBROIDERED GEORGETTE GOWN",
+        price : 1650,
+        desc : "A modern-day fairytale bridal look, this light pink 16-kali georgette lehenga features multi-coloured floral hand embroidery. paired with a matching off-shoulder blouse, it comes with an embroidered tulle dupatta as well as a delicate tulle veil. keep the jewellery subtle with hints of pearl.",
+        img : "/assets/images/products/imgg20.jpg"
+    },
+    {
+        id : 20,
+        title : "RUST GEOMETRIC PRINT DRESS",
+        price : 2250,
+        desc : "A modern-day fairytale bridal look, this light pink 16-kali georgette lehenga features multi-coloured floral hand embroidery. paired with a matching off-shoulder blouse, it comes with an embroidered tulle dupatta as well as a delicate tulle veil. keep the jewellery subtle with hints of pearl.",
+        img : "/assets/images/products/imgg21.jpg"
+    },
+]
+
+export default DATA;
